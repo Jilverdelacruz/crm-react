6dc502b9a0933f6e30d772f6c737ca30088f4b9e
Jilver <jilver.practicando@gmail.com>
2022-08-31 18:03:34 -0500

    validacion nombre

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ function App() {
       <BrowserRouter>
         <Routes>
             <Route path='/clientes' element={<Layout />}>  {/*Cuando tiene una etiqueta de cierre es que agrupo un conjunto de rutas y este se convienrte un master page.*/}
-             <Route index element={<Inicio />}/>  {/*Es una ruta individual, donde se mostrará en la misma ruta del master page, para ello será necesario usar el Outlet, en este caso en el componenete Inicio */}
-             <Route path='nuevo' element={<NuevoCliente />}/> {/* Es una ruta individual donde la ruta se agregará a la ruta principal y tambien verás el master page*/}
-             <Route path='editar/:id' element={<EditarCliente />}/>{/*Para la ruta donde se requiere un id dinámico, :id*/ }
-          </Route>
+              <Route index element={<Inicio />}/>  {/*Es una ruta individual, donde se mostrará en la misma ruta del master page, para ello será necesario usar el Outlet, en este caso en el componenete Inicio */}
+              <Route path='nuevo' element={<NuevoCliente />}/> {/* Es una ruta individual donde la ruta se agregará a la ruta principal y tambien verás el master page*/}
+              <Route path='editar/:id' element={<EditarCliente />}/>{/*Para la ruta donde se requiere un id dinámico, :id*/ }
+            </Route>
         </Routes>
       </BrowserRouter>
       
--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,10 +1,37 @@
-import {Formik, Form, Field} from 'formik'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
+
 const Formulario =() =>{
 
+    const nuevoClienteSchemma = Yup.object().shape({
+        nombre: Yup.string().required('El nombre del cliente es obligatorio'),
+    
+    })
+    
+    const handleSubmit =(valores) =>{
+        console.log(valores)
+    }
     return (
         <div className="mt-5 bg-white px-5 py-8 md:w-3/4 mx-auto shadow-lg rounded-md">
             <h2 className="text-2xl font-bold text-gray-500 text-center"> Agregar Cliente</h2>
-            <Formik>
+            <Formik
+            // Para obtener los valores se usa initialValues
+            initialValues={{
+                nombre:'',
+                empresa:'',
+                email:'',
+                telefono:'',
+                notas:''
+
+            }}
+            // leyendo los valores
+            onSubmit={(valores)=>{
+                handleSubmit(valores)
+            }}
+          
+            validationSchema={nuevoClienteSchemma} // para que se aplique la validación
+            >
+            {({errors, touched}) => (  // el touched es para que se aplique en tiempo real, antes de hacer click en guardar
                 <Form>
                     <div className='my-4'>
                         <label className='text-gray-400 text-lg' htmlFor="nombre">Nombre:</label>
@@ -13,8 +40,13 @@ const Formulario =() =>{
                        id='nombre'
                        placeholder='Ingrese el nombre'
                        type='text'
-                       
+                       name='nombre'
                        />
+                       {errors.nombre && touched.nombre ? ( // el touched se usa cuando quiere tu validacion en tiempo real y no esperar el asubmit
+                        <div className='bg-red-400 mt-2 font-bold py-2 text-center rounded-sm'>
+                            {errors.nombre}
+                        </div>
+                       ) : null}
                     </div>
                     <div className='my-4'>
                         <label className='text-gray-400 text-lg' htmlFor="empresa">Empresa:</label>
@@ -22,7 +54,9 @@ const Formulario =() =>{
                        id='empresa'
                        placeholder='Ingrese el nombre de la Empresa'
                        type='text'
+                       name='empresa'
                        />
+                       
                     </div>
                     <div className='my-4'>
                         <label className='text-gray-400 text-lg' htmlFor="email">E-mail:</label>
@@ -30,6 +64,7 @@ const Formulario =() =>{
                        id='email'
                        placeholder='Ingrese su correo'
                        type='email'
+                       name='email'
                        />
                     </div>
                     <div className='my-4'>
@@ -38,23 +73,27 @@ const Formulario =() =>{
                        id='telefono'
                        placeholder='Ingrese el número telefónico'
                        type='tel'
+                       name='telefono'
                        />
                     </div>
                     <div className='my-4'>
                         <label className='text-gray-400 text-lg' htmlFor="notas">Notas:</label>
                        <Field className='py-3 px-4 block w-full mt-2 placeholder:text-gray-300 border border-gray-300 outline-indigo-500 rounded-md'
                        id='notas'
+                       as='textarea'
                        placeholder='Ingresa la nota'
                        type='text'
+                       name='notas'
                        />
                     </div>
                     <input className='text-lg font-bold text-white bg-indigo-500 uppercase py-3 px-4 rounded-md w-full hover:shadow-lg hover:shadow-indigo-400'
                     value='Guardar'
-                    type='button'
+                    type='submit'
                     
                     />
                     
                 </Form>
+                )}
             </Formik>
         </div>
     )
