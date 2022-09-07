946dfd2557fb85cd90a2f43f5456149afc3cdfd8
Jilver <jilver.practicando@gmail.com>
2022-09-07 18:10:36 -0500

    Vista de lista clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -3,7 +3,7 @@ import ListaClientes from "../componentes/ListaClientes"
 import {useState,useEffect} from 'react'
 const Inicio = () =>{
 
-    const [clientes, SetClientes] = useState([])
+    const [clientes, setClientes] = useState([])
 
     useEffect( ()=>{
         const cargarClientes =(async ()=>{
@@ -11,7 +11,7 @@ const Inicio = () =>{
             const url = 'http://localhost:4000/clientes'  
             const respuesta = await fetch(url)
             const resultado = await respuesta.json()
-            SetClientes(resultado)
+            setClientes(resultado)
             } catch (error) {
                 console.log(error)
             } 
@@ -21,8 +21,8 @@ const Inicio = () =>{
     }, [])
     return (
         <div>
-             <h1 className="font-bold text-4xl text-indigo-500">Lista de Clientes</h1>
-            <p className="text-xl mt-5 font-semibold text-gray-500">Visualización de los clientes registrados</p>
+             <h1 className="font-bold text-4xl text-indigo-500">Clientes</h1>
+            <p className="text-xl mt-5 font-semibold text-gray-500">Administra tus clientes</p>
             < ListaClientes />
             
         </div>
